7ced9167bd8c5176144d2aedcd538fd455efce10
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-26 16:36:33 -0500

    create home page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,22 @@
-class Posts < ApplicationController
+class PostController < ApplicationController
 
+  def new
+    @post = Post.new
+  end
 
-  resources :posts
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 
 end

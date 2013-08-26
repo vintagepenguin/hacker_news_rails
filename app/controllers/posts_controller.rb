945a3279e8080ae3be44db367c71dc152a891945
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-26 16:58:24 -0500

    post action new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,23 @@
-class PostController < ApplicationController
+class PostsController < ApplicationController
 
   def new
     @post = Post.new
   end
 
-  def create
-  end
+  # def create
+  #   @post = Post.create(params[:post])
+  # end
 
-  def show
-  end
+  # def show
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-  end
+  # def update
+  # end
 
-  def destroy
-  end
+  # def destroy
+  # end
 
 end
